1fd01a44128e6ae5f57fd717b77171b4032c4294
kamalnath69 <kamalnath9669@gmail.com>
2024-04-12 18:08:20 +0530

    changes in styling

--- AwesomeProject/App/Screen/LoginScreen/Login.jsx
+++ AwesomeProject/App/Screen/LoginScreen/Login.jsx
@@ -1,70 +1,74 @@
-import { StyleSheet,View, Text, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Colors from '../../Utils/Colors'
 import { useFonts } from "expo-font";
-import { AppRegistry } from 'react-native-web'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'; // Importing responsive screen functions
 
 export default function Login() {
   const [fontsLoaded] = useFonts({
     'mr': require('../../Utils/MarckScriptRegular.ttf'),
-     'qs':require('../../Utils/Quicksand-VariableFont_wght.ttf'),
+    'qs': require('../../Utils/Quicksand-VariableFont_wght.ttf'),
   });
   if (!fontsLoaded) {
     return <Text>Loading...</Text>;
   }
   return (
     <>
-    <View >
-      <Image source={require('./../../../assets/images/HomeLogo.png')}
-        style={styles.homelogo}
-      />
-    </View>
-    <View style={styles.subcon}>
-     <Text style={styles.hometext}>Welcome to Crowdify</Text>
-     <Text style={styles.hometext2}>STAY CONNECTED WITH PEOPLE</Text>
-     <TouchableOpacity style={styles.button} onPress={()=>console.log("button clicked")}>
-      <Text style={{textAlign:"center",color:Colors.dg,fontSize:17,fontFamily:'mr'}}>Let's Get Started</Text>
-     </TouchableOpacity>
-    </View>
+      <View style={styles.container}>
+        <Image
+          source={require('./../../../assets/images/HomeLogo.png')}
+          style={styles.homelogo}
+        />
+      </View>
+      <View style={styles.subcon}>
+        <Text style={styles.hometext}>Welcome to Crowdify</Text>
+        <Text style={styles.hometext2}>STAY CONNECTED WITH PEOPLE</Text>
+        <TouchableOpacity style={styles.button} onPress={() => console.log("button clicked")}>
+          <Text style={{ textAlign: "center", color: Colors.dg, fontSize: wp('4.5%'), fontFamily: 'mr' }}>Let's Get Started</Text>
+        </TouchableOpacity>
+      </View>
     </>
   )
 }
 
 const styles = StyleSheet.create({
-  homelogo:{
-    width:230,
-    height:230,
-    marginTop:100
+  container: {
+    alignItems: 'center',
   },
-  hometext2:{
-    fontSize:20,
-    fontFamily:'qs',
-    color:Colors.lg,
-    alignSelf:'center',
-    width:150
+  homelogo: {
+    width: wp('60%'), 
+    height: hp('20%'),
   },
-  hometext:{
-    alignSelf:'center',
-    padding:25,
-    fontSize:35,
-    fontFamily:'mr',
+  hometext2: {
+    fontSize: wp('5%'),
+    fontFamily: 'qs',
     color:'white',
-    marginTop:10
+    alignSelf: 'center',
+    paddingLeft: wp('5%'),
+    paddingRight: wp('5%'),
   },
-  subcon:{
-    width:'100%',
-    backgroundColor:Colors.dg,
-    height:'40%',
-    marginTop:150,
-    borderTopLeftRadius:30,
-    borderTopRightRadius:30,
+  hometext: {
+    alignSelf: 'center',
+    paddingBottom: wp('5%'),
+    paddingLeft: wp('5%'),
+    paddingRight: wp('5%'),
+    fontSize: wp('8%'),
+    fontFamily: 'mr',
+    color: 'white', 
   },
-  button:{
-    alignSelf:'center',
-    backgroundColor:'white',
-    padding:15,
-    width:'80%',
-    marginTop:40,
-    borderRadius:50,
+  subcon: {
+    backgroundColor: Colors.dg,
+    alignItems: 'center', 
+    borderRadius: wp('10%'), 
+    paddingTop: hp('10%'), 
+  },
+  button: {
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    padding: wp('3%'), 
+    width: wp('60%'), 
+    marginTop: hp('5%'),
+    marginBottom: hp('10%'), 
+    borderRadius: wp('10%'), 
   }
 })
